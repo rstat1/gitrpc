19304788f4a2a5459a418bb1cf536fa20fc78c1a
rstat1 <rstat1@gmail.com>
2019-03-15 02:27:59 -0400

    Kick the dc repo indexer in the face.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace nexus;
 
 int main(int argc, char* argv[]) {
 	writeToLog("Init", true);
-	LOG_FROM_HERE("Version: %s from the %s branch", BUILDNUMBER, BRANCH)
+	LOG_FROM_HERE("GitRPC Version: %s from the %s branch", BUILDNUMBER, BRANCH)
 
 	AppImpl* app = new AppImpl();
 	app->Main();
